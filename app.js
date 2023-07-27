56c9d55dbb47d999ec3b2a09815e4b031bdbf558
Mukul Upadhyaya <mukul.upadhyaya.it.2018@miet.ac.in>
2023-07-27 23:04:20 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,8 @@
+const heading = React.createElement("div", {id: "heading", xyz:"abc"}, 
+                    React.createElement("div", {id: "child", xyz:"abc"},
+                       [React.createElement("h1",{}, "Hello beta!"),
+                        React.createElement("h2",{},"Kese ho??")]));
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+console.log(heading);
+root.render(heading);
