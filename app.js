b40d50c83983523f3896cf11213799531a0add61
Mukul Upadhyaya <mukul.upadhyaya.it.2018@miet.ac.in>
2023-07-29 18:38:12 +0530

    Iginiting our App

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const heading = React.createElement("div", {id: "heading", xyz:"abc"}, 
                     React.createElement("div", {id: "child", xyz:"abc"},
                        [React.createElement("h1",{}, "Hello beta!"),
