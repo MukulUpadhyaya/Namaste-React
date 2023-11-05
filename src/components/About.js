fef6dfb3c579cb118cefa5546bda88bbaba9a053
Mukul Upadhyaya <mukul.upadhyaya.it.2018@miet.ac.in>
2023-11-05 21:58:55 +0530

    Application name changes

--- src/components/About.js
+++ src/components/About.js
@@ -4,7 +4,7 @@ export const About = () => {
         <div className=" p-4 bg-orange-50">
           <h1 className="text-3xl font-bold">About Us</h1>
           <p className="mt-5 text-lg">
-            Welcome to <span className="font-bold ">Munch Mate</span> , your go-to
+            Welcome to <span className="font-bold ">FoodForFoodies</span> , your go-to
             destination for delicious food delivered right to your doorstep! We
             are passionate about food and dedicated to providing you with a
             delightful dining experience.
@@ -13,7 +13,7 @@ export const About = () => {
         <div className="bg-gray-200 p-4">
           <h1 className="text-3xl font-bold">Our Story</h1>
           <p className="mt-5 text-lg">
-            Munch Mate was founded with a simple yet powerful mission: to connect
+            FoodForFoodies was founded with a simple yet powerful mission: to connect
             food lovers with exceptional restaurants, bringing you a diverse
             selection of cuisines from around the world. Our journey began with
             the belief that great food should be easily accessible to everyone, no
@@ -26,7 +26,7 @@ export const About = () => {
             Have questions, suggestions, or feedback? We'd love to hear from you!
             Feel free to contact our customer support team through our app, or
             reach out to us via email at Munchmate@xyz.com. Thank you for choosing
-            Munch Mate as your food delivery partner. We look forward to serving
+            FoodForFoodies as your food delivery partner. We look forward to serving
             you and satisfying your culinary cravings. Bon appétit!
           </p>
         </div>
@@ -36,13 +36,13 @@ export const About = () => {
       //   <h1 className="font-bold text-2xl mt-6 text-center">About Us</h1>
       //   <div className="bg-gray-300 mt-6 w-3/5">
       //     <p className="px-4 text-xl">
-      //       Welcome to <span className="font-bold ">Munch Mate</span> , your go-to
+      //       Welcome to <span className="font-bold ">FoodForFoodies</span> , your go-to
       //       destination for delicious food delivered right to your doorstep! We
       //       are passionate about food and dedicated to providing you with a
       //       delightful dining experience.
       //     </p>
       //     <p className="mt-6 px-4 text-xl">
-      //       <span className="font-bold ">Munch Mate</span> was founded with a
+      //       <span className="font-bold ">FoodForFoodies</span> was founded with a
       //       simple yet powerful mission: to connect food lovers with exceptional
       //       restaurants, bringing you a diverse selection of cuisines from around
       //       the world. Our journey began with the belief that great food should be
