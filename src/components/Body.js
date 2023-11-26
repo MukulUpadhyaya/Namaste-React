12d05beed264b342cc1f93ec391f44197ac763bb
Mukul Upadhyaya <mukul.upadhyaya.it.2018@miet.ac.in>
2023-11-26 18:04:50 +0530

    Fixed CORS issue

--- src/components/Body.js
+++ src/components/Body.js
@@ -18,7 +18,7 @@ const Body = () => {
 
   const fetchData = async () => {
     const data = await fetch(
-      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.9844618&lng=77.7064137&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+      "https://corsproxy.io/?https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.9844618&lng=77.7064137&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
     );
     console.log("Hello");
 
